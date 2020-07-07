3c89b31454d397d58ca0085ac2220ef0fa81f4b7
Taylor Allen <taylorallen0913@gmail.com>
2020-07-06 17:11:23 -0700

    feat: added and configured prettier

--- packages/react-management-ui/webpack.config.js
+++ packages/react-management-ui/webpack.config.js
@@ -1,24 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    devServer: {
-        contentBase: path.resolve(__dirname, './public'),
-        historyApiFallback: true
-    },
-    entry: path.resolve(__dirname, './src/index.tsx'),
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx$/,
-                use: "babel-loader"
-            }
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "bundle.js"
-    }
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+    historyApiFallback: true,
+  },
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
 };
