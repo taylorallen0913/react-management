c4f1ceb1d91f081c6a6cd7430f7c53d5f009d33c
Taylor Allen <taylorallen0913@gmail.com>
2020-07-06 22:43:00 -0700

    moved project to src folder

--- bin/react-management.js
+++ bin/react-management.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 
 if (args[0] === 'ui') {
   // Launch UI
-  require('../webpack/devServer');
+  require('../src/webpack/devServer');
 } else {
   console.log('Error!');
 }
