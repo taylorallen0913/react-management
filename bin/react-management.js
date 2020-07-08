36473a41896befcd449e05073106c0b62961fa32
Taylor Allen <taylorallen0913@gmail.com>
2020-07-08 14:54:31 -0700

    fixed cli bug

--- bin/react-management.js
+++ bin/react-management.js
@@ -1,14 +1,11 @@
 #!/usr/bin/env node
 
 const args = process.argv.slice(2);
-const { startDevServer } = require('../src/webpack');
 
 if (args[0] === 'ui') {
   // Start server
   require('../src/server');
 
   // Launch UI
-  startDevServer();
-} else {
-  console.log('Error!');
+  require('../src/webpack').startDevServer();
 }
